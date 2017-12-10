8ee2374ce300f5056496f33de2bcdddd31ef646b
trofinata <33377131+trofinata@users.noreply.github.com>
2017-12-10 15:48:34 +0300

    Финальная проверка

--- js/popup.js
+++ js/popup.js
@@ -7,6 +7,7 @@
   var nickname = popup.querySelector("[name=feedback-name]");
   var email = popup.querySelector("[name=feedback-email]");
   var proposal = popup.querySelector("[name=feedback-proposal]");
+  var text = popup.querySelector("[placeholder]");
 
   var storage = localStorage.getItem("nickname");
 
@@ -28,16 +29,58 @@
     popup.classList.remove("modal-error");
   });
 
-  form.addEventListener("submit", function (evt) {
+ form.addEventListener("submit", function (evt) {
     if (!nickname.value || !email.value || !proposal.value) {
       evt.preventDefault();
       popup.classList.remove("modal-error");
       popup.offsetWidth = popup.offsetWidth;
-      popup.classList.add("modal-error");
+      popup.classList.add("modal-error");                             
     } else {
-      localStorage.setItem("nickname", nickname.value);
+      localStorage.setItem("nickname", nickname.value);      
     }    
   });
+
+  form.addEventListener("submit", function (evt) {
+    if (!nickname.value){      
+      nickname.classList.add("modal-light");
+    }    
+    if (nickname.value){      
+      nickname.classList.remove("modal-light");                                   
+    }  
+  });
+
+  nickname.addEventListener("click", function (evt) {
+    nickname.classList.remove("modal-light");     
+    }
+  );
+
+ form.addEventListener("submit", function (evt) {
+    if (!email.value){       
+      email.classList.add("modal-light");                             
+    } 
+    if (email.value){      
+      email.classList.remove("modal-light");                             
+    }     
+  });
+
+ email.addEventListener("click", function (evt) {
+    email.classList.remove("modal-light");     
+    }
+  );
+
+  form.addEventListener("submit", function (evt) {
+    if (!proposal.value){      
+      proposal.classList.add("modal-light");                             
+    }  
+    if (proposal.value){      
+      proposal.classList.remove("modal-light");                             
+    }   
+  });
+
+  proposal.addEventListener("click", function (evt) {
+    proposal.classList.remove("modal-light");     
+    }
+  );
 
   window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
@@ -47,3 +90,4 @@
       }
     }
   }); 
+
